85eb544c6ae31f75072476e1a7060a2f654366ad
sathishkumarmsk <sathishkumarmsk5699@gmail.com>
2022-01-19 16:19:48 +0530

    Get by id api

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import res from "express/lib/response";
 
 const router = Router();
 
@@ -12,11 +13,31 @@ const data = [
         createdOn: new Date(),
 
 
+    },
+    {
+        id: 2,
+        FirstName: "sathish",
+        LastName: "msk",
+        UserName: "msk_sathish",
+        Password: true,
+        createdOn: new Date(),
+
+
     }
 ]
 
 router.get('/', (req, res) => {
     res.status(200).json(data);
 });
+
+router.get('/:id', (req, res) => {
+    const findData = data.find(item => item.id === parseInt(req.params.id));
+    if (findData) {
+        res.status(200).json(findData);
+
+    } else {
+        res.sendStatus(404);
+    }
+})
 
 export default router;
